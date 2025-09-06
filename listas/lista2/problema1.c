7859cbf6015afccea3c738c6cd32068fe88194f5
Felipe Duarte <felipeaugustoduarte2@gmail.com>
2025-09-06 20:35:29 -0300

    Lista 2: Problema 1

---
+++ listas/lista2/problema1.c
@@ -0,0 +1,28 @@
+#include <stdio.h> // Inclui biblioca stdio.h (Entrada e Saída)
+#include <math.h> // Inclui biblioteca math.h para utilizar a função pow()
+
+int main() { // Função principal do código
+
+    float peso, altura, imc; // Declaração de variáveis de peso, altura e imc do tipo float (Números reais)
+
+    printf("Digite o seu peso em quilogramas:\n"); // Texto de instrução ao usuário
+    scanf("%f", &peso); // Leitura da entrada do usuário, atribuindo o valor para a variável peso
+    printf("Digite a sua altura em metros:\n"); // Texto de instrução ao usuário
+    scanf("%f", &altura); // Leitura da entrada do usuário, atribuindo o valor para a variável altura
+
+    imc = peso / (pow(altura, 2)); // Cálculo do IMC
+
+    printf("O seu IMC (Indice de Massa Corporal) e: %.2f\n", imc); // Imprime na tela o resultado do IMC, com duas casas decimais de aproximação
+
+    if (imc < 18.5) { // Condicional para verificar se o IMC está abaixo de 18.5
+        printf("Sua classificacao com base no IMC: Abaixo do peso");
+    } else if (imc >= 18.5 && imc < 25) { // Condicional para verificar se o IMC está entre 18.5 e 25
+        printf("Sua classificacao com base no IMC: Peso normal"); 
+    } else if (imc >= 25 && imc < 30) { // Condicional para verificar se o IMC está entre 25 e 30
+        printf("Sua classificacao com base no IMC: Sobrepeso");
+    } else { // Caso o IMC não se enquadre nas condições acima quer dizer que ele está acima de 30, logo o bloco de código abaixo que roda
+        printf("Sua classificacao com base no IMC: Obesidade");
+    }
+
+    return 0; // Retorna 0
+}
