e35c70c9089db1fce145a1caee4511b1758e4b24
Felipe Duarte <felipeaugustoduarte2@gmail.com>
2025-09-26 11:06:41 -0300

    feat: menu de opções

---
+++ projeto/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    
+    int opcao_menu;
+
+    printf("Digite a opcao desejada:\n 1 - Cadastro\n 2 - Consulta\n 3 - Atualizar Cadastro\n 4 - Deletar Cadastro\n 5 - Sair\n");
+    scanf("%d", &opcao_menu);
+
+    switch (opcao_menu) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            printf("Opcao invalida!");
+    }
+}
