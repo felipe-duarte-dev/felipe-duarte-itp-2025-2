779206d331ed7bca4176f4a2205b7527e5990543
Felipe Duarte <felipeaugustoduarte2@gmail.com>
2025-09-28 16:22:06 -0300

    feat: função para alteração de cadastro

--- projeto/src/main.c
+++ projeto/src/main.c
@@ -5,7 +5,7 @@ typedef struct { // Estrutura que servirá de molde para as informações pertin
     char nome[50]; // Todo aluno possui com nome com tamanho de até 50 caracteres
     int idade; // Todo aluno possui uma idade inteira
     int serie_escolar; // Todo aluno está em uma série escolar
-    char matricula[11]; // Todo aluno recebe uma matrícula do tipo: Ano-Perído-Classe_escolar-Serie_escolar-Número_identificador, ex: 20251033001
+    char matricula[12]; // Todo aluno recebe uma matrícula do tipo: Ano-Perído-Classe_escolar-Serie_escolar-Número_identificador, ex: 20251033001
 } Aluno; // O exemplo acima descreve um aluno matriculado no período 2025.1, ensino médio: 03, 3 ano: 3, número identificador: 001
 
 void limpar_buffer() { // Função para limpar o buffer de entrada
@@ -36,7 +36,7 @@ void cadastro_aluno(Aluno lista[], int *total_alunos) { // Função para cadastr
     limpar_buffer(); // Limpa o buffer para a entrada seguinte
 
     printf("Digite a matricula: \n"); // Instrução para o usuário
-    scanf("%s", lista[indice].matricula); // Utilizei o scanf pois a entrada da matrícula não contém espaços, a legibilidade do código com scanf é mais simples
+    scanf("%12s", lista[indice].matricula); // Utilizei o scanf pois a entrada da matrícula não contém espaços, a legibilidade do código com scanf é mais simples
     limpar_buffer(); // Mesmo sendo a última pergunta a limpeza do buffer se faz necessária para continuar utilizando o loop do menu
 
     (*total_alunos)++; // Incrementa a variável que guarda o número de alunos cadastrados
@@ -104,6 +104,77 @@ void consultar_alunos(Aluno lista[], int total_alunos) { // Função para a list
 
 }
 
+void alterar_aluno(Aluno lista[], int total_alunos) {
+
+    if (total_alunos == 0) { // Verifica se há alunos cadastrados
+        printf("\nNao ha alunos cadastrados!\n"); // Imprime aviso caso não haja alunos cadastrados
+        return; // Retorna a função
+    }
+
+    int opcao_alterar, indice_alterar, nova_idade, nova_serie; // Declaração de variáveis do tipo inteiro
+    char novo_nome[50]; // Declaração de variável do tipo string (cadeia de caracteres)
+    char nova_matricula[12]; // Declaração de variável do tipo string (cadeia de caracteres)
+
+    printf("Digite o numero do cadastro a ser alterado: \n"); // Instrução para o usuário
+    scanf("%d", &indice_alterar); // Lê e armazena o número do cadastro
+    indice_alterar = indice_alterar - 1; // Decrementa a variável em 1 unidade para refletir o índice real no vetor, já que o primeiro índice é o 0
+    limpar_buffer(); // Limpa o buffer de entrada
+
+    if (indice_alterar < 0 || indice_alterar >= total_alunos) { // Verifica se o indice digitado está dentro dos índices existentes
+        printf("Aluno inexistente, digite um numero entre 1 e %d para uma alteracao valida!", total_alunos); // Exibe mensagem de erro
+        return; // Retorna a função sem nada
+    }
+
+    do { // Laço de repetição das opções
+
+        printf("Qual campo deseja alterar: \n 1 - Nome\n 2 - Idade\n 3 - Serie\n 4 - Matricula\n 5 - Sair\n"); // Opções de alteração
+        scanf("%d", &opcao_alterar); // Lê e armazena a opção de alteração
+        limpar_buffer(); // Limpa buffer de entrada
+
+        if (opcao_alterar != 1 && opcao_alterar != 2 && opcao_alterar != 3 && opcao_alterar != 4 && opcao_alterar != 5) { // Condição que verifica se a opção é válida
+            opcao_alterar = -1; // Caso não seja válida o valor -1 é atribuído a variável de opção
+        }
+
+        switch (opcao_alterar) { // Executa a opção correspondente a escolhida pelo usuário
+            case 1:
+                printf("Digite o novo nome: \n"); // Instrução para o usuário
+                fgets(novo_nome, 50, stdin); // Lê a entrada do usuário, incluindo espaços em branco
+                novo_nome[strcspn(novo_nome, "\n")] = 0; // Retira do final o "\n" que o fgets adiciona
+                strcpy(lista[indice_alterar].nome, novo_nome); // Utiliza a função strcpy para alterar o valor da chave nome do cadastro escolhido
+                printf("Novo nome: %s\n", lista[indice_alterar].nome); // Imprime o novo nome para o usuário conferir
+                break;
+            case 2:
+                printf("Digite a nova idade: \n"); // Instrução para o usuário
+                scanf("%d", &nova_idade); // Lê nova idade
+                limpar_buffer(); // Limpa o buffer de entrada
+                lista[indice_alterar].idade = nova_idade; // Altera o valor da chave idade do cadastro escolhido
+                printf("Nova idade: %d\n", lista[indice_alterar].idade); // Imprime nova idade
+                break;
+            case 3:
+                printf("Digite a nova serie escolar: \n"); // Instrução para o usuário
+                scanf("%d", &nova_serie); // Lê a nova série escolar
+                limpar_buffer(); // Limpa buffer de entrada
+                lista[indice_alterar].serie_escolar = nova_serie; // Altera o valor da chave série escolar do cadastro escolhido
+                printf("Nova serie escolar: %d\n", lista[indice_alterar].serie_escolar); // Imprime nova série escolar
+                break;
+            case 4:
+                printf("Digite a nova matricula: \n"); // Instrução para o usuário
+                scanf("%12s", nova_matricula); // Lê nova matrícula
+                limpar_buffer(); // Limpa o buffer de entrada
+                strcpy(lista[indice_alterar].matricula, nova_matricula); // Utiliza a função da lib string.h para alterar o valor da chave matricula do cadastro escolhido
+                printf("Nova matricula: %s\n", lista[indice_alterar].matricula); // Imprime nova matrícula
+                break;
+            case 5: // Caso para sair
+                return; // Retorna a função sem nada
+                break;
+            default: // O -1 cai nesse caso
+                printf("Opcao invalida! Tente novamente\n"); // Exibe mensagem de erro
+        }
+
+    } while (opcao_alterar != 5); // O laço se repete até que a opção 5 seja escolhida
+
+}
+
 int main() { // Função principal
 
     Aluno lista_alunos[100]; // Vetor de dados do tipo Aluno, utilizando struct, possui o tamanho máximo de 100 elementos
@@ -128,6 +199,7 @@ int main() { // Função principal
                 consultar_alunos(lista_alunos, total_alunos); // Chama a função de listagem de usuários
                 break;
             case 3: // Opção de atualização de usuário
+                alterar_aluno(lista_alunos, total_alunos); // Chama a função para a alteração de alunos
                 break;
             case 4: // Opção de deletar um usuário
                 break;
